41a6aad025fffe939f144b7f8fd35cf42e090d68
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-02-10 17:14:16 +0100

    add: ex

--- app.js
+++ app.js
@@ -15,7 +15,30 @@ const port = 3000
 
 
 /*attivare*/
-app.get('/', (req, res) => {
+app.get('/api/blog', (req, res) => {
+
+    const blog = [
+        {
+
+            title: "Cracker alla barbabietola",
+      
+            content: `I cracker alla barbabietola sono uno snack stuzzicante e originale da preparare in casa utilizzando ingredienti semplici e genuini. Queste sfogliette dal colore brillante non passeranno inosservate nel vostro cestino del pane e arricchiranno la tavola con il loro gusto unico e accattivante. I cracker fatti a mano sono anche un gustoso snack spezza fame, da portare in ufficio o a scuola. Venite a scoprire il nostro mix di semi e cereali per realizzare l'impasto e divertitevi a sperimentare nuove ricette di cracker variando i semi, le farine e le spezie per gusti sempre nuovi, ecco qualche idea:
+      
+                      Cracker di farro
+      
+                      Cracker di lupini
+      
+                      Cracker allo zafferano
+      
+                      Cracker ai semi`,
+      
+            image: "cracker_barbabietola.avif",
+      
+            tags: ["Antipasti", "Ricette vegetariane", "Ricette al forno"],
+      
+          },
+      
+    ]
 
 
 
